ea6f24780087b97fc3d5af6fb09fb235ae286a23
nagahama_kouki <k021g1319@g.neec.ac.jp>
2022-06-23 23:26:40 +0900

    add 弾の発射

---
+++ PlayerBullet.cpp
@@ -0,0 +1,28 @@
+#include "PlayerBullet.h"
+
+void PlayerBullet::Initialize(Model* model, const Vector3& position) { 
+	assert(model);
+
+	model_ = model;
+	//テクスチャ読み込み
+	textureHandle_ = TextureManager::Load("Black.png");
+
+	//ワールド変換の初期化
+	worldTransform_.Initialize();
+
+	//引数で受け取った初期座標をセット
+	worldTransform_.translation_ = position;
+
+	
+}
+
+void PlayerBullet::Update() {
+	//行列更新
+	AffinTrans::affin(worldTransform_);
+
+	worldTransform_.TransferMatrix();
+}
+
+void PlayerBullet::Draw(const ViewProjection& viewProjection) {
+	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+}
